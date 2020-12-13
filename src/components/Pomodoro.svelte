012c583eaed9aa240ea6156b92f30d99261e94dd
handleman <pavel.handleman@gmail.com>
2020-12-13 20:05:49 +0300

    cleanup

---
+++ src/components/Pomodoro.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+<style lang="scss">
+    $side: 250px;
+    .pomodoro{
+        width: $side;
+        height: $side;
+        border-radius: 150px;
+        background-color: crimson;
+        box-shadow: 3px 3px 18px -6px rgba(0,0,0,0.75);
+        display: flex;
+        .clock{
+            text-align: center;
+            font-size: 40pt;
+            font-weight: bold;
+            color: white;
+        }
+    }
+    
+</style>
+
+<div class="pomodoro">
+    <div class="clock">
+        <span >25</span>:<span>00</span>
+    </div>
+</div>
